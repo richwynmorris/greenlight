fd3b345d42b3a66ce8dbc8befb76f4bc637354d6
richwynmorris <richwynmorris@protonmail.com>
2022-09-12 10:03:21 +0100

    refactor: json marshalling into helper method

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,12 +1,21 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+	// Test string with interpolated values using the %q verb.
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+
+	err := app.writeJSON(w, http.StatusOK, data, nil)
+	if err != nil {
+		app.logger.Printf(err.Error())
+		http.Error(w, "failed to marshall json data and formulate response", http.StatusBadRequest)
+		return
+	}
 }
