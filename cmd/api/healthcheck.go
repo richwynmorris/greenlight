1a0d187a30c40502dbebc64bde1053857f416059
richwynmorris <richwynmorris@protonmail.com>
2022-09-13 09:30:07 +0100

    refactor: use envelope type to contextuzlise json data

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -6,13 +6,15 @@ import (
 
 func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	// Test string with interpolated values using the %q verb.
-	data := map[string]string{
-		"status":      "available",
-		"environment": app.config.env,
-		"version":     version,
+	env := envelope{
+		"status": "available",
+		"system_info": map[string]string{
+			"environment": app.config.env,
+			"version":     version,
+		},
 	}
 
-	err := app.writeJSON(w, http.StatusOK, data, nil)
+	err := app.writeJSON(w, http.StatusOK, env, nil)
 	if err != nil {
 		app.logger.Printf(err.Error())
 		http.Error(w, "failed to marshall json data and formulate response", http.StatusBadRequest)
