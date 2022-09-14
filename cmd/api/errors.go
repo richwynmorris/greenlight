b1f2ca95f8f762302b7b22938c003a8f54ed5d37
richwynmorris <richwynmorris@protonmail.com>
2022-09-14 07:59:17 +0100

    fix: use pointers onf http.Request

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -9,7 +9,7 @@ func (app *application) logError(r http.Request, err error) {
 	app.logger.Print(err)
 }
 
-func (app *application) errorResponse(w http.ResponseWriter, r http.Request, status int, message any) {
+func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, status int, message any) {
 	env := envelope{"message": message}
 
 	err := app.writeJSON(w, status, env, nil)
@@ -19,17 +19,17 @@ func (app *application) errorResponse(w http.ResponseWriter, r http.Request, sta
 	}
 }
 
-func (app *application) serverNotFoundResponse(w http.ResponseWriter, r http.Request) {
+func (app *application) serverErrorResponse(w http.ResponseWriter, r *http.Request) {
 	msg := "The server is unable to fulfil this request at this time. Please try again later."
 	app.errorResponse(w, r, http.StatusInternalServerError, msg)
 }
 
-func (app *application) resourceNotFoundResponse(w http.ResponseWriter, r http.Request) {
+func (app *application) resourceNotFoundResponse(w http.ResponseWriter, r *http.Request) {
 	msg := "The resource requested could not be found"
 	app.errorResponse(w, r, http.StatusNotFound, msg)
 }
 
-func (app *application) methodNotAllowedResponse(w http.ResponseWriter, r http.Request) {
+func (app *application) methodNotAllowedResponse(w http.ResponseWriter, r *http.Request) {
 	msg := fmt.Sprintf("The method %s is not allowed for this request", r.Method)
 	app.errorResponse(w, r, http.StatusMethodNotAllowed, msg)
 }
