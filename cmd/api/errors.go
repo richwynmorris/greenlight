ff12f8739b4de2fe3a65fde207e1c136c55313b3
richwynmorris <richwynmorris@protonmail.com>
2022-09-14 07:56:58 +0100

    feat: add helper methods for formatting errors

---
+++ cmd/api/errors.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (app *application) logError(r http.Request, err error) {
+	app.logger.Print(err)
+}
+
+func (app *application) errorResponse(w http.ResponseWriter, r http.Request, status int, message any) {
+	env := envelope{"message": message}
+
+	err := app.writeJSON(w, status, env, nil)
+	if nil != err {
+		app.logError(r, err)
+		w.WriteHeader(500)
+	}
+}
+
+func (app *application) serverNotFoundResponse(w http.ResponseWriter, r http.Request) {
+	msg := "The server is unable to fulfil this request at this time. Please try again later."
+	app.errorResponse(w, r, http.StatusInternalServerError, msg)
+}
+
+func (app *application) resourceNotFoundResponse(w http.ResponseWriter, r http.Request) {
+	msg := "The resource requested could not be found"
+	app.errorResponse(w, r, http.StatusNotFound, msg)
+}
+
+func (app *application) methodNotAllowedResponse(w http.ResponseWriter, r http.Request) {
+	msg := fmt.Sprintf("The method %s is not allowed for this request", r.Method)
+	app.errorResponse(w, r, http.StatusMethodNotAllowed, msg)
+}
