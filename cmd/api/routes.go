db59b091281aaabee4335237352f1f8c0501ae86
richwynmorris <richwynmorris@protonmail.com>
2022-09-14 08:10:48 +0100

    refactor: override default error handling on routher with helper funcs

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -10,6 +10,11 @@ func (app *application) routes() *httprouter.Router {
 	// Initialise a new router
 	router := httprouter.New()
 
+	// Override defaults error handling on router by replacing them with helper function that satisfy the http.Handler
+	// interface.
+	router.NotFound = http.HandlerFunc(app.resourceNotFoundResponse)
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthCheckHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
