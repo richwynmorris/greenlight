c9e0efaab9d64a4d505de593e910e087c5beb7fc
richwynmorris <richwynmorris@protonmail.com>
2022-09-19 17:52:43 +0100

    feat: add psql database and connection pool to config

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	_ "github.com/lib/pq"
 )
 
 // version is a string containing the application version number.
@@ -16,6 +20,9 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn string
+	}
 }
 
 // application holds the handlers, helpers and middleware to support the application's functionality.
@@ -30,6 +37,8 @@ func main() {
 	// Initialise flag names and default values to run the application.
 	flag.IntVar(&cfg.port, "port", 4000, "API Server Port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgreSQL DSN")
+
 	// Parses the flag values and sets them to the config fields.
 	flag.Parse()
 
@@ -37,6 +46,15 @@ func main() {
 	   time and date.
 	*/
 	logger := log.New(os.Stdout, "", log.LstdFlags)
+
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatalf(err.Error())
+	}
+
+	defer db.Close()
+
+	logger.Printf("database connection pool established")
 
 	// Declare instance of application struct with logger and config settings.
 	app := &application{
@@ -58,8 +76,30 @@ func main() {
 
 	// Start the server.
 	logger.Printf("Starting %s server on port: %d", cfg.env, cfg.port)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		logger.Fatalf(err.Error())
 	}
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	// Create an empty connection pool using the dsn config.
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create a context that will time out after 5 seconds.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+
+	defer cancel()
+
+	// Pass the context to the PingContext function to check that the db is working correctly
+	// and can be connected to within 5 seconds.
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
