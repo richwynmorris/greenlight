044f4b4987308cd92f723d8489bc66260b357451
richwynmorris <richwynmorris@protonmail.com>
2022-09-19 16:38:32 +0100

    feat: add validation checks for json request

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"richwynmorris.co.uk/internal/data"
+	"richwynmorris.co.uk/internal/validator"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -19,6 +20,28 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 	err := app.readJSON(w, r, &input)
 	if err != nil {
 		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	v := validator.New()
+
+	v.Check(input.Title != "", "title", "title must be provided")
+	v.Check(len(input.Title) <= 500, "title", "title must not be more than 500 bytes long")
+
+	v.Check(input.Year != 0, "year", "year must be provided")
+	v.Check(input.Year >= 1888, "year", "year must be greater than 1888")
+	v.Check(input.Year <= int32(time.Now().Year()), "year", "year cannot be in the future")
+
+	v.Check(input.Runtime != 0, "runtime", "runtime must be provided")
+	v.Check(input.Runtime > 0, "runtime", "runtime must be a positive integer")
+
+	v.Check(input.Genres != nil, "genres", "a genre must be selected")
+	v.Check(len(input.Genres) >= 1, "genres", "a minimum of one genre must be selected")
+	v.Check(len(input.Genres) <= 5, "genres", "There cannot be more than 5 genres selected")
+	v.Check(validator.Unique(input.Genres), "genres", "genres cannot contain duplicates")
+
+	if !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
 
