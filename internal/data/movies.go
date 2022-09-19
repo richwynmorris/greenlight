cdb18aa1ae815369f2bd61011d5c9c67fc077c87
richwynmorris <richwynmorris@protonmail.com>
2022-09-19 16:51:26 +0100

    refactor: move movie validation checks to own function ValidateMovie

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -1,6 +1,10 @@
 package data
 
-import "time"
+import (
+	"time"
+
+	"richwynmorris.co.uk/internal/validator"
+)
 
 type Movie struct {
 	ID        int64     `json:"id,omitempty"`
@@ -10,4 +14,21 @@ type Movie struct {
 	Runtime   Runtime   `json:"runtime,omitempty"`
 	Genres    []string  `json:"genres,omitempty"`
 	Version   int32     `json:"version"`
+}
+
+func ValidateMovie(v *validator.Validator, input *Movie) {
+	v.Check(input.Title != "", "title", "title must be provided")
+	v.Check(len(input.Title) <= 500, "title", "title must not be more than 500 bytes long")
+
+	v.Check(input.Year != 0, "year", "year must be provided")
+	v.Check(input.Year >= 1888, "year", "year must be greater than 1888")
+	v.Check(input.Year <= int32(time.Now().Year()), "year", "year cannot be in the future")
+
+	v.Check(input.Runtime != 0, "runtime", "runtime must be provided")
+	v.Check(input.Runtime > 0, "runtime", "runtime must be a positive integer")
+
+	v.Check(input.Genres != nil, "genres", "a genre must be selected")
+	v.Check(len(input.Genres) >= 1, "genres", "a minimum of one genre must be selected")
+	v.Check(len(input.Genres) <= 5, "genres", "There cannot be more than 5 genres selected")
+	v.Check(validator.Unique(input.Genres), "genres", "genres cannot contain duplicates")
 }
