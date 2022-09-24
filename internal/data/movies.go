24cea25d4319be026581676c11cabf987e8951b1
richwynmorris <richwynmorris@protonmail.com>
2022-09-24 14:02:49 +0100

    feat: add Get and Insert methods to the movies db model

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -2,7 +2,10 @@ package data
 
 import (
 	"database/sql"
+	"errors"
 	"time"
+
+	"github.com/lib/pq"
 
 	"richwynmorris.co.uk/internal/validator"
 )
@@ -38,12 +41,42 @@ type MovieModel struct {
 	DB *sql.DB
 }
 
-func (m MovieModel) insert(movie *Movie) error {
-	return nil
+func (m MovieModel) Insert(movie *Movie) error {
+	query := `
+	INSERT INTO movies (title, year, runtime, genres)
+	VALUES ($1, $2, $3, $4)
+	RETURNING id, created_at, version`
+
+	args := []any{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres)}
+
+	return m.DB.QueryRow(query, args...).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
 }
 
 func (m MovieModel) Get(id int64) (*Movie, error) {
-	return nil, nil
+	query := `SELECT (id, created_at, version, title, runtime, genres)
+			  FROM movies
+			  WHERE id = $1;`
+
+	var movie Movie
+
+	err := m.DB.QueryRow(query, id).Scan(
+		&movie.ID,
+		&movie.Version,
+		&movie.Year,
+		&movie.Title,
+		&movie.Runtime,
+		&movie.CreatedAt,
+		pq.Array(&movie.Genres))
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrRecordNotFound
+		default:
+			return nil, err
+		}
+	}
+
+	return &movie, nil
 }
 func (m MovieModel) Update(movie Movie) error {
 	return nil
