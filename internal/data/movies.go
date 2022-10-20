821ee23e224cc887807ea64d586e80fc88e90278
richwynmorris <richwynmorris@protonmail.com>
2022-10-20 09:36:00 +0100

    feat: add version check and custom error for race condition when updating a record

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -86,7 +86,7 @@ func (m MovieModel) Get(id int64) (*Movie, error) {
 func (m MovieModel) Update(movie *Movie) error {
 	query := `UPDATE movies
 			  SET title = $1, year = $2, runtime = $3, genres = $4, version = version + 1
-			  WHERE id = $5
+			  WHERE id = $5 AND version = $6
               RETURNING version`
 
 	args := []any{
@@ -95,6 +95,7 @@ func (m MovieModel) Update(movie *Movie) error {
 		movie.Runtime,
 		pq.Array(movie.Genres),
 		movie.ID,
+		movie.Version,
 	}
 
 	// QueryRow expects to return a single row from the db, if it doesn't it throws and error.
@@ -106,7 +107,7 @@ func (m MovieModel) Update(movie *Movie) error {
 	if err != nil {
 		switch {
 		case errors.Is(err, sql.ErrNoRows):
-			return ErrRecordNotFound
+			return ErrEditConflict
 		default:
 			return err
 		}
