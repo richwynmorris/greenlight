87340afa82fb4119f371edb2d4e404b6a8be9f82
richwynmorris <richwynmorris@protonmail.com>
2022-10-26 08:20:59 +0100

    feat: add GetAll call and pass in filter fields

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -194,7 +194,7 @@ func (app *application) listMoviesHandler(w http.ResponseWriter, r *http.Request
 		return
 	}
 
-	movies, err := app.models.Movies.GetAll()
+	movies, err := app.models.Movies.GetAll(input.Title, input.Genres, input.Filters)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 		return
