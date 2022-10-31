a61fc9aaf5948a6fdaff076587cd3b2740102d94
richwynmorris <richwynmorris@protonmail.com>
2022-10-31 09:20:40 +0000

    feat: add filter methods for sorting and pagination; extend getAll query

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,6 +1,10 @@
 package data
 
-import "richwynmorris.co.uk/internal/validator"
+import (
+	"strings"
+
+	"richwynmorris.co.uk/internal/validator"
+)
 
 type Filters struct {
 	Page         int
@@ -13,7 +17,33 @@ func ValidateFilters(v *validator.Validator, f Filters) {
 	v.Check(f.Page > 0, "page", "must be greater than zero")
 	v.Check(f.Page <= 10_000_000, "page", "maximum per page is 10,000,000.")
 	v.Check(f.PageSize > 0, "page_size", "must be greater than zero.")
-	v.Check(f.PageSize <= 10_000_000, "page_size", "maximum page size is 10,000,000")
+	v.Check(f.PageSize <= 100, "page_size", "maximum page size is 100")
 
 	v.Check(validator.PermittedValue(f.Sort, f.SortSafeList...), "sort", "invalid sort value")
+}
+
+func (f Filters) sortColumn() string {
+	for _, safeValue := range f.SortSafeList {
+		if f.Sort == safeValue {
+			return strings.TrimPrefix(f.Sort, "-")
+		}
+	}
+	// Validate Filters has already passed out filters, we panic as a failsafe in the instance of SQL injections attack
+	// occuring.
+	panic("unsafe sort parameter:" + f.Sort)
+}
+
+func (f Filters) sortDirection() string {
+	if strings.HasPrefix(f.Sort, "-") {
+		return "DESC"
+	}
+	return "ASC"
+}
+
+func (f Filters) limit() int {
+	return f.PageSize
+}
+
+func (f Filters) offset() int {
+	return (f.Page - 1) * f.PageSize
 }
--- internal/data/movies.go
+++ internal/data/movies.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"fmt"
 	"time"
 
 	"github.com/lib/pq"
@@ -150,18 +151,21 @@ func (m MovieModel) Delete(id int64) error {
 }
 
 func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*Movie, error) {
-	query := `SELECT id, created_at, title, year, runtime, genres, version
+	query := fmt.Sprintf(`SELECT id, created_at, title, year, runtime, genres, version
 			  FROM movies
 			  WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1 ) OR $1 = '')
 			  AND (genres @> $2 OR $2 = '{}')
-			  ORDER BY id`
+			  ORDER BY %s %s, id ASC
+			  LIMIT $3 OFFSET $4`, filters.sortColumn(), filters.sortDirection())
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
+	args := []any{title, pq.Array(genres), filters.limit(), filters.offset()}
+
 	var movies []*Movie
 
-	rows, err := m.DB.QueryContext(ctx, query, title, pq.Array(genres))
+	rows, err := m.DB.QueryContext(ctx, query, args...)
 	if err != nil {
 		return nil, err
 	}
