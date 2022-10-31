2f31740067a4ab24e039fefcbe180ff10ce0ed6e
richwynmorris <richwynmorris@protonmail.com>
2022-10-31 09:48:13 +0000

    feat: add metadata helper; add count to query; add metadata json to resp

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -150,8 +150,9 @@ func (m MovieModel) Delete(id int64) error {
 	return nil
 }
 
-func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*Movie, error) {
-	query := fmt.Sprintf(`SELECT id, created_at, title, year, runtime, genres, version
+func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*Movie, Metadata, error) {
+	query := fmt.Sprintf(
+		`SELECT count(*) OVER(), id, created_at, title, year, runtime, genres, version
 			  FROM movies
 			  WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1 ) OR $1 = '')
 			  AND (genres @> $2 OR $2 = '{}')
@@ -163,17 +164,19 @@ func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*M
 
 	args := []any{title, pq.Array(genres), filters.limit(), filters.offset()}
 
-	var movies []*Movie
-
 	rows, err := m.DB.QueryContext(ctx, query, args...)
 	if err != nil {
-		return nil, err
+		return nil, Metadata{}, err
 	}
+
+	totalRecords := 0
+	var movies []*Movie
 
 	for rows.Next() {
 		var movie Movie
 
 		err = rows.Scan(
+			&totalRecords,
 			&movie.ID,
 			&movie.CreatedAt,
 			&movie.Title,
@@ -183,15 +186,17 @@ func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*M
 			&movie.Version,
 		)
 		if err != nil {
-			return nil, err
+			return nil, Metadata{}, err
 		}
 
 		movies = append(movies, &movie)
 	}
 	err = rows.Err()
 	if err != nil {
-		return nil, err
+		return nil, Metadata{}, err
 	}
 
-	return movies, nil
+	metadata := calculateMetadata(totalRecords, filters.Page, filters.PageSize)
+
+	return movies, metadata, nil
 }
