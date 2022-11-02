eb91e9775e7c831a0f655aa44161caba433374d0
richwynmorris <richwynmorris@protonmail.com>
2022-11-02 08:36:10 +0000

    feat: implement panic recovery

---
+++ cmd/api/middleware.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (app *application) recoverPanic(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Creates a deferred IIFE to be run in the instance that the server
+		// encounters a panic. The panic is dealt with gracefully by sending a
+		// 500 response to the client and logging the fatal error encountered.
+		defer func() {
+			err := recover()
+			if err != nil {
+				w.Header().Set("Connection", "close")
+				app.serverErrorResponse(w, r, fmt.Errorf("%s", err))
+			}
+		}()
+		next.ServeHTTP(w, r)
+	})
+}
