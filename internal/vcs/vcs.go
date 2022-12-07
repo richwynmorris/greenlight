44c12a4b005101b21be6567139fd8348cad86836
richwynmorris <richwynmorris@protonmail.com>
2022-12-07 08:39:00 +0000

    feat: add time to version control

--- internal/vcs/vcs.go
+++ internal/vcs/vcs.go
@@ -8,11 +8,14 @@ import (
 func Version() string {
 	var revision string
 	var modified bool
+	var time string
 
 	bi, ok := debug.ReadBuildInfo()
 	if ok {
 		for _, s := range bi.Settings {
 			switch s.Key {
+			case "vcs.time":
+				time = s.Value
 			case "vcs.revision":
 				revision = s.Value
 			case "vcs.modified":
@@ -24,8 +27,8 @@ func Version() string {
 	}
 
 	if modified {
-		return fmt.Sprintf("%s-dirty", revision)
+		return fmt.Sprintf("%s-%s-dirty", time, revision)
 	}
 
-	return revision
+	return fmt.Sprintf("%s-%s", time, revision)
 }
